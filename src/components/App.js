6e7d8e7486c29bc76ca2fc84cbbb79e26a237099
Debbie30 <cherotichdebby11@gmail.com>
2024-04-28 03:38:05 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,33 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import Links from "./Links"
+import { name, city, bio, github, linkedin } from '../data/user';
+
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
 
+
 function App() {
+  const user = {
+    name: 'Liza',
+    city: 'New York',
+    bio: 'I made this!',
+    color: 'firebrick',
+    links: {
+      github: 'https://github.com/liza',
+      linkedin: 'https://www.linkedin.com/in/liza/'
+    }
+  };
+
   return (
-    <div>
+    <div className="App">
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} />
+      <Links github={user.links.github} linkedin={user.links.linkedin} />
     </div>
   );
 }
